47de6e109b2c1ea39d7597500a8f76c2482c4c10
Fernando García Martínez <fernando@oc2763461342.ibm.com>
2017-07-04 12:11:36 +0200

    fix and debug

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -x
 [ -z "${REMOTE_SERVICE}" ] || [ -z "${EXPORTED_PORT}" ] && exit  1
 
 sed -i \
     "s/{{ REMOTE_SERVICE }}/${REMOTE_SERVICE}/g" \
-    /etc/nginx/nginx.conf && \
+    /etc/nginx/nginx.conf
+
 sed -i \
     "s/{{ EXPORTED_PORT }}/${EXPORTED_PORT}/g" \
-    /etc/nginx/nginx.conf && \
+    /etc/nginx/nginx.conf
+
 nginx -t && \
-exec nginx &
+    exec nginx &
